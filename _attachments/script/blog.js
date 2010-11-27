da1a43ba2a1f8bddbb429dbabe5fc7ec5b803f03
Max Ogden <max@maxogden.com>
2010-11-26 18:42:22 -0800

    pretty dates, fixing nav

--- _attachments/script/blog.js
+++ _attachments/script/blog.js
@@ -1,4 +1,5 @@
 function render(data, target, callback) {
+  if (data.date) data.date = new Date(data.date).toDateString();
   var container = $("#" + target + "s");
   var template = $('#' + target + '-template').text();
   container.append(Mustache.to_html(template, data));
@@ -11,8 +12,14 @@ var blog = $.sammy(function() {
     this.redirect(article);
   })
 
-  this.get('#:id', function() {
-    $.couch.db('blog').openDoc(this.params['id'], {success: function(data){
+  this.get('#/:nav', function() {
+    var nav = $('#' + this.params['nav']);
+    nav.siblings().hide();
+    nav.show();
+  })
+
+  this.get('#/blog/:id', function() {
+    $.couch.db('blog').openDoc(this.params['id'], {success: function(data) {
       $('#blogposts').html('');
       render(data, 'blogpost', function(container) {
         $('#blognav').css({'height': container.height()});
@@ -29,13 +36,12 @@ var blog = $.sammy(function() {
 });
 
 $(function() {
-  $.couch.db('blog').view("blog/nav", {success: function(data) {
+  $.couch.db('blog').view("blog/nav", {descending: true, success: function(data) {
     $.each(data.rows, function(i, data) {
       render($.extend(data.value, {id: data.id}), "blognav-post");
     })
     blog.run('#/');
   }})
-  
 });
 
 var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");

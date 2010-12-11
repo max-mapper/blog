4f4bdea8f3de1fbba74a82764b6390ae332f6120
Max Ogden <max@maxogden.com>
2010-12-10 17:39:28 -0800

    remove ability to edit comments

--- validate_doc_update.js
+++ validate_doc_update.js
@@ -1,4 +1,9 @@
 function (newDoc, oldDoc, userCtx, secObj) {
   var v = require("vendor/couchapp/lib/validate").init(newDoc, oldDoc, userCtx, secObj);
-  if (!v.isAdmin()) v.unauthorized("Only admins can change stuff");
+  v.require(['type']);
+  if (newDoc.type == "comment") {
+    if (oldDoc) v.unauthorized("You can't edit comments");    
+  } else {    
+    if (!v.isAdmin()) v.unauthorized("Only admins can change stuff");
+  }
 }

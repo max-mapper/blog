7faac2bb85c2e866155c71b3b433ae89a1b539a8
Max Ogden <mogden@gmail.com>
2012-10-08 16:35:58 +0200

    convert to flat files

---
+++ Makefile
@@ -0,0 +1,1 @@
+all: node render.js
--- download-posts.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs')
-var _ = require('underscore')
-
-var syncHelpers = require('./sync-helpers')
-
-syncHelpers.getPublishedPosts('maxogden', function(err, data) {
-  if (err) return console.error(err)
-  fs.writeFileSync('./posts.json', JSON.stringify(data))
-})
